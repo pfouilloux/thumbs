9d3777bbe1cfb2893204e7592a32ba1a544b47b0
Pierre Fouilloux <pierre.fouilloux@safetyculture.io>
2023-05-04 19:24:25 +1000

    2.3 - Test harness - Refactor

--- internal/img/crop_test.go
+++ internal/img/crop_test.go
@@ -16,14 +16,7 @@ type scenario struct {
 }
 
 func TestCrop(t *testing.T) {
-	baseImg := image.NewRGBA(image.Rect(0, 0, 1000, 1000))
-	size := baseImg.Bounds().Size()
-	for x := 0; x < size.X; x++ {
-		for y := 0; y < size.Y; y++ {
-			clr := color.RGBA{R: uint8(255 * x / size.X), G: uint8(255 * y / size.Y), B: 55, A: 255}
-			baseImg.Set(x, y, clr)
-		}
-	}
+	baseImg := newGradientImage(1000, 1000)
 
 	scns := map[string]scenario{
 		"should not crop image with same size as target": {
@@ -45,12 +38,39 @@ func (scn scenario) testCrop(t *testing.T) {
 
 	got := img.Crop(scn.src, scn.targetWidth, scn.targetHeight)
 
-	// we check the size of the images to tell us if we've cropped an image of the right size
-	if scn.want.Bounds() != got.Bounds() {
+	scn.assertIsExpectedSize(t, got)
+	scn.assertContainsExpectedPixels(t, got)
+}
+
+func (scn scenario) assertIsExpectedSize(t testing.TB, got image.Image) {
+	if scn.want.Bounds().Min != got.Bounds().Min || scn.want.Bounds().Max != got.Bounds().Max {
 		t.Errorf("expected an %dx%d image but got an %dx%d image", scn.want.Bounds().Dx(), scn.want.Bounds().Dy(), got.Bounds().Dx(), got.Bounds().Dx())
 	}
-	
+}
+
+func (scn scenario) assertContainsExpectedPixels(t testing.TB, got image.Image) {
 	if !reflect.DeepEqual(scn.want, got) {
 		t.Errorf("the cropped image was not the same as the expected subimage")
+	}
+}
+
+func newGradientImage(w, h int) *image.RGBA {
+	gradient := image.NewRGBA(image.Rect(0, 0, w, h))
+	size := gradient.Bounds().Size()
+	for x := 0; x < size.X; x++ {
+		for y := 0; y < size.Y; y++ {
+			gradient.Set(x, y, interpolateColour(size, x, y))
+		}
+	}
+
+	return gradient
+}
+
+func interpolateColour(sz image.Point, x, y int) color.RGBA {
+	return color.RGBA{
+		R: uint8(255 * x / sz.X),
+		G: uint8(255 * y / sz.Y),
+		B: 55,
+		A: 255,
 	}
 }
